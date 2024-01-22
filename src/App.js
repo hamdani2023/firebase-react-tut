048ff66c4a1c4afe70372b311f5338913506463c
hamdani <mustapha.hamdani@gmail.com>
2024-01-22 18:31:23 +0100

    view update from firestore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
+import { getDefaultNormalizer } from "@testing-library/react";
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      <p> Tut firebase</p>
+       {users.map((user) => {
+        return (
+          <div>
+            {" "}
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
